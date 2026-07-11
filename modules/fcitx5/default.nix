6686bf5d7c17de4247323179ff341ec1b628de2a
InfiniteNightmare <742851870@qq.com>
2026-07-12 00:13:37 +0800

    Manage Fcitx5 settings declaratively
    Move the profile, classic UI, XIM, and Xiaohe double-pinyin settings into Home Manager and remove obsolete generated configuration and theme assets.

--- modules/fcitx5/default.nix
+++ modules/fcitx5/default.nix
@@ -1,43 +1,6 @@
 { pkgs, ... }:
 
 {
-  home.file.".config/fcitx5/profile" = {
-    source = ./profile;
-    # every time fcitx5 switch input method, it will modify ~/.config/fcitx5/profile,
-    # so we need to force replace it in every rebuild to avoid file conflict.
-    force = true;
-  };
-  home.file.".config/fcitx5/classicui.conf".source = ./classicui.conf;
-  home.file.".config/fcitx5/xim.conf" = {
-    source = ./xim.conf;
-    force = true;
-  };
-  home.file.".config/fcitx5/conf/pinyin.conf" = {
-    source = ./pinyin.conf;
-    force = true;
-  };
-
-  # = {
-  #   "fcitx5/profile" = {
-  #     source = ./profile;
-  #     # every time fcitx5 switch input method, it will modify ~/.config/fcitx5/profile,
-  #     # so we need to force replace it in every rebuild to avoid file conflict.
-  #     force = true;
-  #   };
-  #   # "fcitx5/conf/classicui.conf".source = ./classicui.conf;
-  #   # "fcitx5/conf/xim.conf" = {
-  #   #   source = ./xim.conf;
-  #   #   force = true;
-  #   # };
-  # };
-
-  # xdg.dataFile = {
-  #   "fcitx5/themes" = {
-  #     source = ./themes;
-  #     recursive = true;
-  #   };
-  # };
-
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
@@ -51,5 +14,147 @@
       # fcitx5-mozc    # japanese input method
       fcitx5-gtk # gtk im module
     ];
+    fcitx5.settings = {
+      inputMethod = {
+        GroupOrder."0" = "Default";
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "shuangpin";
+        };
+        "Groups/0/Items/0" = {
+          Name = "keyboard-us";
+          Layout = "us";
+        };
+        "Groups/0/Items/1" = {
+          Name = "shuangpin";
+          Layout = null;
+        };
+      };
+
+      addons = {
+        classicui.globalSection = {
+          "Vertical Candidate List" = "False";
+          PerScreenDPI = "True";
+          WheelForPaging = "True";
+          TrayOutlineColor = "#000000";
+          TrayTextColor = "#ffffff";
+          PreferTextIcon = "False";
+          ShowLayoutNameInIcon = "True";
+          UseInputMethodLangaugeToDisplayText = "True";
+          ForceWaylandDPI = 0;
+        };
+
+        xim.globalSection = {
+          UseOnTheSpot = "True";
+        };
+
+        pinyin.globalSection = {
+          ShuangpinProfile = "Xiaohe";
+          UseShuangpin = "True";
+          ShowShuangpinMode = "True";
+          PageSize = 7;
+          SpellEnabled = "True";
+          SymbolsEnabled = "True";
+          ChaiziEnabled = "True";
+          ExtBEnabled = "True";
+          StrokeCandidateEnabled = "True";
+          CloudPinyinEnabled = "True";
+          CloudPinyinIndex = 2;
+          CloudPinyinAnimation = "True";
+          KeepCloudPinyinPlaceHolder = "False";
+          PreeditMode = "Composing pinyin";
+          PreeditCursorPositionAtBeginning = "True";
+          PinyinInPreedit = "True";
+          Prediction = "False";
+          PredictionSize = 49;
+          BackspaceBehaviorOnPrediction = "Backspace when not using on-screen keyboard";
+          SwitchInputMethodBehavior = "Commit current preedit";
+          SecondCandidate = "";
+          ThirdCandidate = "";
+          UseKeypadAsSelection = "False";
+          BackSpaceToUnselect = "True";
+          "Number of sentence" = 2;
+          WordCandidateLimit = 15;
+          LongWordLengthLimit = 4;
+          QuickPhraseKey = "semicolon";
+          VAsQuickphrase = "True";
+          FirstRun = "False";
+        };
+
+        pinyin.sections = {
+          ForgetWord."0" = "Control+7";
+
+          PrevPage = {
+            "0" = "minus";
+            "1" = "Up";
+            "2" = "KP_Up";
+            "3" = "Page_Up";
+          };
+
+          NextPage = {
+            "0" = "equal";
+            "1" = "Down";
+            "2" = "KP_Down";
+            "3" = "Next";
+          };
+
+          PrevCandidate."0" = "Shift+Tab";
+          NextCandidate."0" = "Tab";
+
+          CurrentCandidate = {
+            "0" = "space";
+            "1" = "KP_Space";
+          };
+
+          CommitRawInput = {
+            "0" = "Return";
+            "1" = "KP_Enter";
+            "2" = "Control+Return";
+            "3" = "Control+KP_Enter";
+            "4" = "Shift+Return";
+            "5" = "Shift+KP_Enter";
+            "6" = "Control+Shift+Return";
+            "7" = "Control+Shift+KP_Enter";
+          };
+
+          ChooseCharFromPhrase = {
+            "0" = "bracketleft";
+            "1" = "bracketright";
+          };
+
+          FilterByStroke."0" = "grave";
+
+          QuickPhraseTriggerRegex = {
+            "0" = ".(/|@)$";
+            "1" = "^(www|bbs|forum|mail|bbs)\\.";
+            "2" = "^(http|https|ftp|telnet|mailto):";
+          };
+
+          Fuzzy = {
+            VE_UE = "True";
+            NG_GN = "True";
+            Inner = "True";
+            InnerShort = "True";
+            PartialFinal = "True";
+            PartialSp = "False";
+            V_U = "False";
+            AN_ANG = "False";
+            EN_ENG = "False";
+            IAN_IANG = "False";
+            IN_ING = "False";
+            U_OU = "False";
+            UAN_UANG = "False";
+            C_CH = "False";
+            F_H = "False";
+            L_N = "False";
+            L_R = "False";
+            S_SH = "False";
+            Z_ZH = "False";
+            Correction = "None";
+          };
+        };
+      };
+    };
   };
 }
